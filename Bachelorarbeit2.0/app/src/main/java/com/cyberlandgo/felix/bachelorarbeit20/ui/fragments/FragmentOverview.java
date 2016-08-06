70fa10a68b18afd50f4eb5f9e92a7a456ebb64be
Cyberlander <felix.froehlich1@gmx.net>
2016-08-06 12:39:44 +0200

    Init Bachelorarbeit mit Tabview

---
+++ Bachelorarbeit2.0/app/src/main/java/com/cyberlandgo/felix/bachelorarbeit20/ui/fragments/FragmentOverview.java
@@ -0,0 +1,32 @@
+package com.cyberlandgo.felix.bachelorarbeit20.ui.fragments;
+
+/**
+ * Created by Felix on 06.08.2016.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cyberlandgo.felix.bachelorarbeit20.R;
+
+public class FragmentOverview extends Fragment
+{
+    public FragmentOverview() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+
+}
