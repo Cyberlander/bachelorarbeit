db0ca49c2ef249fc40270dac394583f2681b51c2
Cyberlander <felix.froehlich1@gmx.net>
2016-08-07 21:09:45 +0200

    Beacon-Erkennung funktioniert (Callback-Methoden)

--- Bachelorarbeit2.0/app/src/main/java/com/cyberlandgo/felix/bachelorarbeit20/application/BillingSystemApplication.java
+++ Bachelorarbeit2.0/app/src/main/java/com/cyberlandgo/felix/bachelorarbeit20/application/BillingSystemApplication.java
@@ -3,6 +3,7 @@ package com.cyberlandgo.felix.bachelorarbeit20.application;
 import android.app.Application;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.cyberlandgo.felix.bachelorarbeit20.Helper.RegionBuilder;
 import com.cyberlandgo.felix.bachelorarbeit20.database.datasources.StationDataSource;
@@ -10,8 +11,10 @@ import com.cyberlandgo.felix.bachelorarbeit20.database.models.Station;
 
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,17 +25,22 @@ import java.util.List;
 public class BillingSystemApplication extends Application implements BootstrapNotifier
 {
     Preferences preferences;
+    int currentStatusStateMachine;
 
     //die StationDataSource ist die Schnittstelle zu lokalen SQLite-Datenbank
     //in der die Beacon-Regionen zugeordnet zu ihrem Standort gespeichert sind
     StationDataSource stationDataSource;
 
     ArrayList<Station> arrayListStationObjects;
+    List<Region> listRegions;
 
 
     //Android Beacon Library
     //BeaconManager, Schnittstelle der Api zu Beaconerkennung
     BeaconManager beaconManager;
+    //Array von RegionBootstrap-Objekten, die initialisiert werden müssen,
+    //damit die Callbacks ausgelöst werden
+    private RegionBootstrap[] regionBootstrap;
 
 
     @Override
@@ -43,14 +51,26 @@ public class BillingSystemApplication extends Application implements BootstrapNo
         //Ezeugen des Preferences-Objekt und Zuweisung der Application als dessen Kontext
         preferences = new Preferences(this);
 
+
+        //holt sich den aktuellen Status der StateMachine
+        currentStatusStateMachine = Preferences.getStatusStateMachine();
+
+        //holt über die StationDatasource Stationsobjekte aus der Datenbank
         arrayListStationObjects = getListOfStationObjectsFromDatabase();
+
+        listRegions = RegionBuilder.getRegions(arrayListStationObjects);
 
 
         initializeBeaconManager();
 
-        List<Region> listRegions = RegionBuilder.getRegions(arrayListStationObjects);
-        Log.e("??????????????","?????????");
-        Log.e("!!!!!!!!!!!!!",listRegions.toString());
+        regionBootstrap = new RegionBootstrap[listRegions.size()];
+
+        for (int i = 0; i < listRegions.size(); i++) {
+            regionBootstrap[i] = new RegionBootstrap(this, listRegions.get(i));
+        }
+
+
+
 
 
 
@@ -83,6 +103,21 @@ public class BillingSystemApplication extends Application implements BootstrapNo
     @Override
     public void didEnterRegion(Region region)
     {
+        Identifier currentMajorIdentifier = region.getId2();
+        Identifier currentMinorIdentifier = region.getId3();
+
+        //aktuelle Identifier in Strings umwandeln
+        String currentMajorIdentifierString = "" + currentMajorIdentifier;
+        String currentMinorIdentifierString = "" + currentMinorIdentifier;
+
+
+        if (currentStatusStateMachine == StateMachine.STATUS_NOT_RUNNING)
+        {
+            Toast.makeText(getApplicationContext(), currentMajorIdentifierString +":" +currentMajorIdentifier,Toast.LENGTH_LONG).show();
+            Log.e("!!!!!!!!!!",currentMajorIdentifierString +":" +currentMajorIdentifier);
+
+
+        }
 
     }
 
@@ -95,6 +130,7 @@ public class BillingSystemApplication extends Application implements BootstrapNo
     @Override
     public void didExitRegion(Region region)
     {
+        Log.e("!!!!!!!!!!","flies away");
 
     }
 
@@ -121,10 +157,11 @@ public class BillingSystemApplication extends Application implements BootstrapNo
         //dem Beacon-Parser das Layout für iBeacons übergeben
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
-
         // setzen der Scan-Intervalle, hoch setzen, damit
         //schneller iBeacons erkannt werden
         setScanPeriodsForGroundBackground();
+
+
     }
 
 
