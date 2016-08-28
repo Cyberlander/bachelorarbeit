883bed7f9dbb58c08ecdff8a2a488ff61322b929
Cyberlander <felix.froehlich1@gmx.net>
2016-08-28 22:43:56 +0200

    Koordinaten eingefügt

--- Bachelorarbeit2.0/app/src/main/java/com/cyberlandgo/felix/bachelorarbeit20/database/MySQLiteHelper.java
+++ Bachelorarbeit2.0/app/src/main/java/com/cyberlandgo/felix/bachelorarbeit20/database/MySQLiteHelper.java
@@ -174,31 +174,31 @@ public class MySQLiteHelper extends SQLiteOpenHelper
         {
                 //Erstellen der U2 GPSCoordinates-Stationen-Objekte
                 StationGPSCoordinates[] gpscoordinates_stationen_u2 = new StationGPSCoordinates[25];
-                gpscoordinates_stationen_u2[0] = new StationGPSCoordinates(Values.STATION_U2_NIENDORF_NORD, Values.MAJOR_ID_TRAIN,"20000");
-                gpscoordinates_stationen_u2[1] = new StationGPSCoordinates(Values.STATION_U2_SCHIPPELSWEG, Values.MAJOR_ID_TRAIN,"20001");
-                gpscoordinates_stationen_u2[2] = new StationGPSCoordinates(Values.STATION_U2_JOACHIM_MAEHL_STRASSE, Values.MAJOR_ID_TRAIN,"20002");
-                gpscoordinates_stationen_u2[3] = new StationGPSCoordinates(Values.STATION_U2_NIENDORF_MARKT, Values.MAJOR_ID_TRAIN,"20003");
-                gpscoordinates_stationen_u2[4] = new StationGPSCoordinates(Values.STATION_U2_HAGENDEEL, Values.MAJOR_ID_TRAIN,"20004");
-                gpscoordinates_stationen_u2[5] = new StationGPSCoordinates(Values.STATION_U2_HAGENBECKS_TIERPARK, Values.MAJOR_ID_TRAIN,"20005");
-                gpscoordinates_stationen_u2[6] = new StationGPSCoordinates(Values.STATION_U2_LUTTHEROTHSTRASSE, Values.MAJOR_ID_TRAIN,"20006");
-                gpscoordinates_stationen_u2[7] = new StationGPSCoordinates(Values.STATION_U2_OSTERSTRASSE, Values.MAJOR_ID_TRAIN,"20007");
-                gpscoordinates_stationen_u2[8] = new StationGPSCoordinates(Values.STATION_U2_EMILIENSTRASSE, Values.MAJOR_ID_TRAIN,"20008");
-                gpscoordinates_stationen_u2[9] = new StationGPSCoordinates(Values.STATION_U2_CHRISTUSKIRCHE, Values.MAJOR_ID_TRAIN,"20009");
-                gpscoordinates_stationen_u2[10] = new StationGPSCoordinates(Values.STATION_U2_SCHLUMP, Values.MAJOR_ID_TRAIN,"20010");
-                gpscoordinates_stationen_u2[11] = new StationGPSCoordinates(Values.STATION_U2_MESSEHALLEN, Values.MAJOR_ID_TRAIN,"20011");
-                gpscoordinates_stationen_u2[12] = new StationGPSCoordinates(Values.STATION_U2_GAENSEMARKT, Values.MAJOR_ID_TRAIN,"20012");
-                gpscoordinates_stationen_u2[13] = new StationGPSCoordinates(Values.STATION_U2_JUNGFERNSTIEG, Values.MAJOR_ID_TRAIN,"20013");
-                gpscoordinates_stationen_u2[14] = new StationGPSCoordinates(Values.STATION_U2_HAUPTBAHNHOF_NORD, Values.MAJOR_ID_TRAIN,"20014");
-                gpscoordinates_stationen_u2[15] = new StationGPSCoordinates(Values.STATION_U2_BERLINER_TOR, Values.MAJOR_ID_TRAIN,"20015");
-                gpscoordinates_stationen_u2[16] = new StationGPSCoordinates(Values.STATION_U2_BURGSTRASSE, Values.MAJOR_ID_TRAIN,"20016");
-                gpscoordinates_stationen_u2[17] = new StationGPSCoordinates(Values.STATION_U2_HAMMER_KIRCHE, Values.MAJOR_ID_TRAIN,"20017");
-                gpscoordinates_stationen_u2[18] = new StationGPSCoordinates(Values.STATION_U2_RAUES_HAUS, Values.MAJOR_ID_TRAIN,"20018");
-                gpscoordinates_stationen_u2[19] = new StationGPSCoordinates(Values.STATION_U2_HORNER_RENNBAHN, Values.MAJOR_ID_TRAIN,"20019");
-                gpscoordinates_stationen_u2[20] = new StationGPSCoordinates(Values.STATION_U2_LEGIENSTRASSE, Values.MAJOR_ID_TRAIN,"20020");
-                gpscoordinates_stationen_u2[21] = new StationGPSCoordinates(Values.STATION_U2_BILLSTEDT, Values.MAJOR_ID_TRAIN,"20021");
-                gpscoordinates_stationen_u2[22] = new StationGPSCoordinates(Values.STATION_U2_MERKENSTRASSE, Values.MAJOR_ID_TRAIN,"20222");
-                gpscoordinates_stationen_u2[23] = new StationGPSCoordinates(Values.STATION_U2_STEINFURTER_ALLEE, Values.MAJOR_ID_TRAIN,"20023");
-                gpscoordinates_stationen_u2[24] = new StationGPSCoordinates(Values.STATION_U2_MUEMMELMANNSBERG, Values.MAJOR_ID_TRAIN,"20024");
+                gpscoordinates_stationen_u2[0] = new StationGPSCoordinates(Values.STATION_U2_NIENDORF_NORD, "53.64084966","9.95021009");
+                gpscoordinates_stationen_u2[1] = new StationGPSCoordinates(Values.STATION_U2_SCHIPPELSWEG, "53.6355879","9.95249105");
+                gpscoordinates_stationen_u2[2] = new StationGPSCoordinates(Values.STATION_U2_JOACHIM_MAEHL_STRASSE, "53.62946276","9.94971657");
+                gpscoordinates_stationen_u2[3] = new StationGPSCoordinates(Values.STATION_U2_NIENDORF_MARKT, "53.61939648","9.95145464");
+                gpscoordinates_stationen_u2[4] = new StationGPSCoordinates(Values.STATION_U2_HAGENDEEL, "53.60383292","9.94523835");
+                gpscoordinates_stationen_u2[5] = new StationGPSCoordinates(Values.STATION_U2_HAGENBECKS_TIERPARK, "53.59249462","9.94421804");
+                gpscoordinates_stationen_u2[6] = new StationGPSCoordinates(Values.STATION_U2_LUTTHEROTHSTRASSE, "53.58200678","9.94774139");
+                gpscoordinates_stationen_u2[7] = new StationGPSCoordinates(Values.STATION_U2_OSTERSTRASSE, "53.57619494","9.95197606");
+                gpscoordinates_stationen_u2[8] = new StationGPSCoordinates(Values.STATION_U2_EMILIENSTRASSE, "53.57165014","9.95279145");
+                gpscoordinates_stationen_u2[9] = new StationGPSCoordinates(Values.STATION_U2_CHRISTUSKIRCHE, "53.56963049","9.9623723");
+                gpscoordinates_stationen_u2[10] = new StationGPSCoordinates(Values.STATION_U2_SCHLUMP, "53.56760693","9.97017431");
+                gpscoordinates_stationen_u2[11] = new StationGPSCoordinates(Values.STATION_U2_MESSEHALLEN, "53.55785984","9.97677469");
+                gpscoordinates_stationen_u2[12] = new StationGPSCoordinates(Values.STATION_U2_GAENSEMARKT, "53.55571845","9.98752499");
+                gpscoordinates_stationen_u2[13] = new StationGPSCoordinates(Values.STATION_U2_JUNGFERNSTIEG, "53.55272286","9.99492788");
+                gpscoordinates_stationen_u2[14] = new StationGPSCoordinates(Values.STATION_U2_HAUPTBAHNHOF_NORD, "53.55333474","10.00527048");
+                gpscoordinates_stationen_u2[15] = new StationGPSCoordinates(Values.STATION_U2_BERLINER_TOR, "53.55299183","10.02447081");
+                gpscoordinates_stationen_u2[16] = new StationGPSCoordinates(Values.STATION_U2_BURGSTRASSE, "53.55580003","10.04191911");
+                gpscoordinates_stationen_u2[17] = new StationGPSCoordinates(Values.STATION_U2_HAMMER_KIRCHE, "53.55556102","10.05558228");
+                gpscoordinates_stationen_u2[18] = new StationGPSCoordinates(Values.STATION_U2_RAUES_HAUS, "53.55421303","10.06616092");
+                gpscoordinates_stationen_u2[19] = new StationGPSCoordinates(Values.STATION_U2_HORNER_RENNBAHN, "53.55383316","10.08384418");
+                gpscoordinates_stationen_u2[20] = new StationGPSCoordinates(Values.STATION_U2_LEGIENSTRASSE, "53.54726652","10.09560728");
+                gpscoordinates_stationen_u2[21] = new StationGPSCoordinates(Values.STATION_U2_BILLSTEDT, "53.54225576","10.10743046");
+                gpscoordinates_stationen_u2[22] = new StationGPSCoordinates(Values.STATION_U2_MERKENSTRASSE, "53.53864458","10.1255064");
+                gpscoordinates_stationen_u2[23] = new StationGPSCoordinates(Values.STATION_U2_STEINFURTER_ALLEE,"53.54093986","10.13795185");
+                gpscoordinates_stationen_u2[24] = new StationGPSCoordinates(Values.STATION_U2_MUEMMELMANNSBERG, "53.52816507","10.14993381");
         }
 
 
